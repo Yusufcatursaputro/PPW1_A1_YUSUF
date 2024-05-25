02a4f603fe648fa02f2407e670df9e86c44efda7
Yusufcatursaputro <161546923+Yusufcatursaputro@users.noreply.github.com>
2024-05-25 19:14:27 +0700

    Add files via upload

---
+++ pertemuan_11/1. variabel.js
@@ -0,0 +1,13 @@
+var title = "Belajar Pemrogaman Javascript";
+
+var site_name = "Petani Kode";
+var visitor_count = 1234;
+
+// membuat variabel dengan kata kunci let
+let price = 15000;
+
+// membuat variabel tanpa awalan apapun
+stock = 12
+
+// variabel bernilai undefined
+var x;
